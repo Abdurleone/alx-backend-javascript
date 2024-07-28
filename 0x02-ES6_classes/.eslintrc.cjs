72c5e713df0b2edeca8ac86354377846d501519d
Abdurleone <abyyyusuf@gmail.com>
2024-07-28 18:14:59 +0300

    fix

--- 0x02-ES6_classes/.eslintrc.cjs
+++ 0x02-ES6_classes/.eslintrc.cjs
@@ -22,15 +22,21 @@ module.exports = {
         'no-shadow': 'off',
         'no-restricted-syntax': [
             'error',
-            'LabeledStatement',
-            'WithStatement',
-            'no-underscore-dangle', ['error', { 'allow': ['_maxStudentsSize'] }],
+            {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain.',
+            },
+            {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
         ],
+        'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
     },
-    overrides:[
+    overrides: [
         {
             files: ['*.js'],
             excludedFiles: 'babel.config.js',
-        }
-    ]
+        },
+    ],
 };
