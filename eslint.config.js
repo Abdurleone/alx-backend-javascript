e2f722c07e47038f958900d12eefe4d8f5fea599
Abdurleone <abyyyusuf@gmail.com>
2024-07-31 19:30:23 +0300

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,44 @@
 // eslint.config.js
-import { Linter } from 'eslint';
-
-export default new Linter.Config({
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'max-classes-per-file': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'LabeledStatement',
-        message: 'Labeled statements are not allowed.',
-      },
-      {
-        selector: 'WithStatement',
-        message: 'With statements are not allowed.',
-      },
+module.exports = {
+    env: {
+        browser: false,
+        es6: true,
+        jest: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:jest/all',
     ],
-  },
-  overrides: [
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ],
-});
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        'max-classes-per-file': 'off',
+        'no-underscore-dangle': 'off',
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'LabeledStatement',
+                message: 'Labeled statements are not allowed.',
+            },
+            {
+                selector: 'WithStatement',
+                message: 'With statements are not allowed.',
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
+    ],
+};
