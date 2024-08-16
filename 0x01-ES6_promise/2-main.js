98e1537ad67eae5459dd34412095a7c643d75d17
Abdurleone <abyyyusuf@gmail.com>
2024-08-16 18:58:49 +0300

    edit

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,4 +1,7 @@
-import handleResponseFromAPI from "./2-then";
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable jest/require-hook */
+// eslint-disable-next-line import/no-named-as-default
+import handleResponseFromAPI from './2-then';
 
 const promise = Promise.resolve();
 handleResponseFromAPI(promise);
